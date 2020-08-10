644aa45acb8bb04f5e7304de026fcf97910a6a42
Elspeth Curtis <elspethcurtis@gmail.com>
2020-08-10 08:37:00 -0400

    added more styling

--- routes/categories.js
+++ routes/categories.js
@@ -15,9 +15,9 @@ router.get('/:category', (async(req, res) => {
             category: req.params.category
         }
     });
-    
+    //if the array is empty (the category doesn't exist) show an error
     if (!recipes.length){
-        res.sendStatus(404);
+		res.sendStatus(404);
     }
     
     res.render("categories/category", {recipes: recipes, title: req.params.category});
